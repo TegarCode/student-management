2a7b7d2cee6f30498270039585cbf91ba0ddaa97
Tegar Oktavianto Simbolon <oktaviantotegar2@gmail.com>
2023-10-02 13:11:05 +0700

    Semua perubahan sesuai tugas

--- routes/student.route.js
+++ routes/student.route.js
@@ -5,17 +5,65 @@ const router = express.Router();
 
 router.use(protect);
 router.get("/", (req, res) => {
-  return res.json({ status: "success", data: STUDENTS });
+  
+  const loggedInUser = req.user;
+
+  // Menyaring data mahasiswa berdasarkan undangan yang dimiliki oleh pengguna saat ini
+  const filteredStudents = STUDENTS.map((student) => {
+    const userRole = student.users_role.find((role) => role.user_id === loggedInUser.id);
+    console.log(student)
+    // Jika pengguna adalah pemilik (owner) atau memiliki undangan ke mahasiswa tersebut
+    if (student.id === loggedInUser.id || userRole) {
+      // Membuat salinan objek mahasiswa dengan perubahan pada properti 'role'
+      const studentCopy = {
+        id: student.id,
+        name: student.name,
+        age: student.age,
+        gender: student.gender,
+        phone_number: student.phone_number,
+        created_by_id: student.created_by_id,
+        role: userRole ? userRole.role : "owner",
+      };
+      return studentCopy;
+    }
+
+    return {
+      id: student.id,
+      name: student.name,
+      age: student.age,
+      gender: student.gender,
+      phone_number: student.phone_number,
+      created_by_id: student.created_by_id,
+      role: [], // Mengubah role ke dalam format array kosong
+    };
+  }).filter((student) => student.role !== null);
+
+  return res.json({ status: "success", data: filteredStudents });
 });
 
-router.post("/", (req, res) => {
-  const newStudent = { id: STUDENTS.length + 1, ...req.body };
+
+
+
+
+router.post("/", protect, (req, res) => {
+  const loggedInUser = req.user;
+
+  const newStudent = {
+    id: STUDENTS.length + 1,
+    ...req.body,
+    created_by_id: loggedInUser.id,
+    users_role: [],
+  };
+
   STUDENTS.push(newStudent);
   return res.json({ status: "success", data: newStudent });
 });
 
-router.put("/:id", (req, res) => {
+
+router.put("/:id", protect, (req, res) => {
   const { id } = req.params;
+  const loggedInUser = req.user;
+
   const indexId = STUDENTS.map((item) => item.id).indexOf(+id);
   if (indexId === -1) {
     return res
@@ -23,16 +71,34 @@ router.put("/:id", (req, res) => {
       .json({ status: "failed", message: "data not found" });
   }
 
-  STUDENTS[indexId] = { id: +id, ...req.body };
+  const student = STUDENTS[indexId];
 
-  return res.json({
-    status: "success",
-    data: req.body,
-  });
+  // Validasi apakah pengguna memiliki role "owner" atau "editor"
+  if (student.created_by_id === loggedInUser.id || (student.users_role && student.users_role.some((role) => role.user_id === loggedInUser.id && role.role === "editor"))) {
+    // Perbarui data mahasiswa dengan data yang dikirim dalam request body
+    const updatedStudentData = {
+      id: +id,
+      ...student, // Gunakan data yang ada pada mahasiswa saat ini
+      ...req.body, // Perbarui data sesuai dengan body request
+    };
+
+    // Perbarui data mahasiswa dalam array STUDENTS
+    STUDENTS[indexId] = updatedStudentData;
+
+    return res.json({
+      status: "success",
+      data: updatedStudentData,
+    });
+  } else {
+    return res.status(403).json({ status: "failed", message: "Access denied" });
+  }
 });
 
-router.delete("/:id", (req, res) => {
+
+router.delete("/:id", protect, (req, res) => {
   const { id } = req.params;
+  const loggedInUser = req.user;
+
   const indexId = STUDENTS.map((item) => item.id).indexOf(+id);
   if (indexId === -1) {
     return res
@@ -40,12 +106,21 @@ router.delete("/:id", (req, res) => {
       .json({ status: "failed", message: "data not found" });
   }
 
-  STUDENTS.splice(indexId, 1);
+  const student = STUDENTS[indexId];
 
-  return res.json({
-    status: "success",
-    message: "student is removed",
-  });
+  // Validasi apakah pengguna adalah pemilik data
+  if (student.created_by_id === loggedInUser.id) {
+    // Hanya pemilik data yang dapat menghapusnya
+    STUDENTS.splice(indexId, 1);
+
+    return res.json({
+      status: "success",
+      message: "student is removed",
+    });
+  } else {
+    return res.status(403).json({ status: "failed", message: "Access denied" });
+  }
 });
+
 
 module.exports = router;
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { USERS, USER_ROLES } = require("../items");
+const { USERS, USER_ROLES, STUDENTS } = require("../items");
 const { protect } = require("../middleware");
 const router = express.Router();
 
@@ -12,7 +12,35 @@ router.get("/", (req, res) => {
   });
 });
 
-router.put("/invite", (req, res) => {});
+router.put("/invite", protect, (req, res) => {
+  // Mendapatkan data dari body permintaan
+  const invite = req.body;
+
+  // Mendapatkan pengguna yang sedang login dari middleware protect
+  const loggedInUser = req.user;
+
+  console.log(loggedInUser);
+
+  // Mengecek apakah pengguna yang sedang login memiliki izin
+  // untuk menginvite user lain ke student dengan id tertentu
+  if (loggedInUser.id === invite.student_id ) {
+    // Jika pengguna memiliki izin, tambahkan data ke STUDENT
+    // dan objek users_role
+    const newdata = {
+      user_id: invite.user_id,
+      role: invite.role,
+    };
+    // Misalnya, tambahkan data ke STUDENT dengan id 1:
+    STUDENTS[0].users_role.push({ newdata });
+
+    // Kemudian kirim respons yang sesuai
+    return res.json({ STUDENTS });
+  } else {
+    // Jika pengguna tidak memiliki izin, kirim pesan error dengan status code 403
+    return res.status(403).json({ message: "Access denied" });
+  }
+});
+
 
 router.get("/:id", protect, (req, res) => {
   const id = req.params.id;
